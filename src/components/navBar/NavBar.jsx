c326c6b14ab5433b3277023e47bfcbc904f117f7
afaqahmad01 <afaq01ahmad@gmail.com>
2024-01-02 18:52:38 +0500

    Updated

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -136,13 +136,13 @@ const NavBar = () => {
             >
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-poppins font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to="/"
                     className="block py-2 px-3 rounded text-[#000000] md:bg-transparent  md:p-0 "
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a
